c0cca6d83f01622cc378d3c6759646dc42a5ab36
Patrik Svoboda <svob.pata@gmail.com>
2019-08-24 21:43:13 +0200

    Added code and README

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # HapticHelperSwift
 
-A description of this package.
+A simple library for giving the user Haptic feedback written in Swift 5.0
+
+## Usage
+To give a haptic feedback, you need to initialize the `Haptic` class and call `.give()` on it.
+Available haptic types: 
+* .extraLight (`UISelectionFeedbackGenerator()`)
+* .light (`UIImpactFeedbackGenerator(style: .light)`)
+* .medium (`UIImpactFeedbackGenerator(style: .medium)`)
+* .heavy (`UIImpactFeedbackGenerator(style: .heavy)`)
+* .rigid (`UIImpactFeedbackGenerator(style: .rigid)`)
+* .success (`UINotificationFeedbackGenerator()notificationOccurred(.success)`)
+* .warning (`UINotificationFeedbackGenerator()notificationOccurred(.warning)`)
+* .error (`UINotificationFeedbackGenerator()notificationOccurred(.error)`)
--- Sources/HapticHelperSwift/HapticHelperSwift.swift
+++ Sources/HapticHelperSwift/HapticHelperSwift.swift
@@ -1,3 +1,74 @@
-struct HapticHelperSwift {
-    var text = "Hello, World!"
+import Foundation
+import UIKit
+enum HapticType {
+    case extraLight
+    case light
+    case medium
+    case heavy
+    case rigid
+    case success
+    case warning
+    case error
+}
+class Haptic {
+    let lightImpactFeedbackgenerator = UIImpactFeedbackGenerator(style: .light)
+    let mediumImpactFeedbackgenerator = UIImpactFeedbackGenerator(style: .medium)
+    let strongImpactFeedbackgenerator = UIImpactFeedbackGenerator(style: .heavy)
+    let rigidImpactFeedbackgenerator = UIImpactFeedbackGenerator(style: .rigid)
+    let selectionFeedbackGenerator = UISelectionFeedbackGenerator()
+    let notificationFeedbackGenerator = UINotificationFeedbackGenerator()
+    var prepared = false
+    func prepare(_ type: HapticType? = nil) {
+        if type != nil {
+            switch type! {
+            case .extraLight:
+                self.selectionFeedbackGenerator.prepare()
+            case .light:
+                self.lightImpactFeedbackgenerator.prepare()
+            case .medium:
+                self.mediumImpactFeedbackgenerator.prepare()
+            case .heavy:
+                self.strongImpactFeedbackgenerator.prepare()
+            case .rigid:
+                self.rigidImpactFeedbackgenerator.prepare()
+            case .success:
+                self.notificationFeedbackGenerator.prepare()
+            case .warning:
+                self.notificationFeedbackGenerator.prepare()
+            case .error:
+                self.notificationFeedbackGenerator.prepare()
+            }
+        } else {
+            self.selectionFeedbackGenerator.prepare()
+            self.lightImpactFeedbackgenerator.prepare()
+            self.mediumImpactFeedbackgenerator.prepare()
+            self.strongImpactFeedbackgenerator.prepare()
+            self.rigidImpactFeedbackgenerator.prepare()
+            self.notificationFeedbackGenerator.prepare()
+        }
+    }
+    func give(_ type: HapticType = .light) {
+        switch type {
+        case .extraLight:
+            self.selectionFeedbackGenerator.selectionChanged()
+        case .light:
+            self.lightImpactFeedbackgenerator.impactOccurred()
+        case .medium:
+            self.mediumImpactFeedbackgenerator.impactOccurred()
+        case .heavy:
+            self.strongImpactFeedbackgenerator.impactOccurred()
+        case .rigid:
+            self.rigidImpactFeedbackgenerator.impactOccurred()
+        case .success:
+            self.notificationFeedbackGenerator.notificationOccurred(.success)
+        case .warning:
+            self.notificationFeedbackGenerator.notificationOccurred(.warning)
+        case .error:
+            self.notificationFeedbackGenerator.notificationOccurred(.error)
+        }
+        print("gave haptics")
+    }
+    init() {
+        self.prepare()
+    }
 }
